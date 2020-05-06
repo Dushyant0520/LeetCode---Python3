979a26a58042940f8f4c0670973d6a7ff10678cb
Dushyant_V <60645385+Dushyant0520@users.noreply.github.com>
2020-05-05 20:23:41 -0700

    Update First_Unique_Character_in_a_String.py

--- First_Unique_Character_in_a_String.py
+++ First_Unique_Character_in_a_String.py
@@ -20,3 +20,35 @@ if __name__ == '__main__':
     s = "sadfmasdf"
     return_target = firstUniqChar(s)
     print(return_target)
+
+    
+"""
+Leetcode Solution:
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        """
+        :type s: str
+        :rtype: int
+        """
+        # build hash map : character and how often it appears
+        count = collections.Counter(s)
+        
+        # find the index
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx     
+        return -1
+        
+Approach 1: Linear time solution
+The best possible solution here could be of a linear time because to ensure that the character is unique you have to check the whole string anyway.
+
+The idea is to go through the string and save in a hash map the number of times each character appears in the string. That would take \mathcal{O}(N)O(N) time, where N is a number of characters in the string.
+
+And then we go through the string the second time, this time we use the hash map as a reference to check if a character is unique or not.
+If the character is unique, one could just return its index. The complexity of the second iteration is \mathcal{O}(N)O(N) as well.
+
+Complexity Analysis:
+Time complexity : O(N) since we go through the string of length N two times.
+Space complexity : O(N) since we have to keep a hash map with N elements.
+"""
